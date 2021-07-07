eb109b0e78da873d017fad32a6214ff61bbab12e
Matthew Leng <mleng89@gmail.com>
2021-07-06 20:42:19 -0400

    First push with bot response

---
+++ src/index.js
@@ -0,0 +1,64 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const dotenv = require('dotenv');
+dotenv.config();
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interaction', async (interaction) => {
+	if (!interaction.isCommand()) return;
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+client.on('message', (message) => {
+	if (message.content === '!ping') {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+	}
+});
+client.on('message', (message) => {
+	const prefix = '420?';
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'args-info') {
+		if (!args.length) {
+			return message.channel.send(`Don't argue with me, ${message.author}!`);
+		}
+
+		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+	}
+});
+
+client.api
+	.applications(process.env.CLIENT_ID)
+	.guilds(process.env.SERVER_ID)
+	.commands.post({
+		data: {
+			name: 'ping',
+			description: 'ping pong!',
+		},
+	});
+
+client.ws.on('INTERACTION_CREATE', async (interaction) => {
+	if (interaction.data === 'ping') {
+		console.log(interaction);
+		return client.api
+			.interactions(interaction.id, interaction.token)
+			.callback.post({
+				data: {
+					type: 4,
+					data: {
+						content: 'hello world!',
+					},
+				},
+			});
+	}
+});
+
+client.login(process.env.DISCORD_TOKEN);
